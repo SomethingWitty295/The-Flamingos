9425c31503b01867008374b287d6340347bd07a8
Corey Pierce <Pierce18@students.rowan.edu>
2021-03-08 13:02:18 -0500

    Add Publisher.cpp file. Non-functional starter code.

---
+++ Publisher.cpp
@@ -0,0 +1,30 @@
+#include <ace/Log_Msg.h>
+
+#include <dds/DdsDcpsInfrastructureC.h>
+#include <dds/DdsDcpsPublicationC.h>
+
+#include <dds/DCPS/Marked_Default_Qos.h>
+#include <dds/DCPS/Service_Participant.h>
+#include <dds/DCPS/WaitSet.h>
+
+#include <dds/DCPS/StaticIncludes.h>
+#ifdef ACE_AS_STATIC_LIBS
+#include <dds/DCPS/RTPS/RtpsDiscovery.h>
+#include <dds/DCPS/transport/rtps_udp/RtpsUdp.h>
+#endif
+
+#include "MessengerTypeSupportImpl.h"
+
+int main(int argc, char *argv[])
+{
+    DDS::DomainParticipantFactory_var dpf = TheParticipantFactoryWithArgs(argc, argv);
+    DDS::DomainParticipant_var participant = dpf->create_participant(42, //domain ID
+                                                                     PARTICIPANT_QOS_DEFAULT,
+                                                                     0, //No listener required
+                                                                     OpenDDS::DCPS::DEFAULT_STATUS_MASK);
+    if (!participant)
+    {
+        std::cerr << "create_participant failed." << std::endl;
+        return 1;
+    }
+}
