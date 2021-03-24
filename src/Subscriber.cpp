a16e03847d5c7be6286f18426ee2795b2821f084
coreyp7 <79476320+coreyp7@users.noreply.github.com>
2021-03-24 15:56:52 -0400

    Fancy Subscriber
    I think I'm so cool. (with lazy receiving function)

--- src/Subscriber.cpp
+++ src/Subscriber.cpp
@@ -22,49 +22,67 @@
 #include <iostream>
 using namespace std;
 
+void printInstructions(int domainID, string username, string topic);
+int receiving(int seconds, DDS::DomainParticipantFactory_var dpf, int domainID, string topicName);
+
 int main(int argc, char *argv[])
 {
     DDS::DomainParticipantFactory_var dpf = TheParticipantFactoryWithArgs(argc, argv);
-    bool receiving = false;
-    int domainId = 0;
-    string username = NULL;
+    int domainID = 42;
+    string username = "Default";
+    string topicName = "Flamingo List";
 
+    std::cout << "------------------------------------\n";
+    std::cout << "|  ___ _            _                |\n";
+    std::cout << "| | __| |__ _ _ __ (_)_ _  __ _ ___  |\n";
+    std::cout << "| | _|| / _` | '  \\| | ' \\/ _` / _ \\ |\n";
+    std::cout << "| |_| |_\\__,_|_|_|_|_|_||_\\__, \\___/ |\n";
+    std::cout << "|   subscriber            |___/      |\n";
+    std::cout << "------------------------------------\n";
+
+    //std::cout << "Username, Topic, and Domain ID are set to default.\n";
 
     while (true)
     {
-        std::cout << "r : Set Receiving\n";
-        std::cout << "t : Set domain ID\n";
-        std::cout << "u : Change username\n";
-        std::cout << "e : Exit subscriber\n";
+        printInstructions(domainID, username, topicName);
 
         char input;
 
         std::cin >> input;
 
-        switch(input) {
-            case 'r':
-                receiving = !receiving;
-                std::cout << "Receiving = " + receiving;
-                std::cout << "\n";
-                break;
-            case 't':
-                setDomainID();
-                break;
-            case 'u':
-                std::cout << "Please set username: ";
-                std::cin >> username;
-                std::cout << "\nUsername set to " + username;
-                break;
-            case 'e':
+        switch (input)
+        {
+        case 'r':
+            std::cout << "Set timeout time in seconds:";
+            int seconds;
+            std::cin >> seconds;
+            std::cout << "\nWaiting...\n";
+            receiving(seconds, dpf, domainID, topicName);
+            break;
+        case 't':
+            std::cout << "Enter desired domain ID: ";
+            std::cin >> domainID;
+            std::cout << "\n";
+            break;
+        case 'u':
+            std::cout << "Please set username: ";
+            std::cin >> username;
+            std::cout << "\nUsername set to " + username;
+            std::cout << "\n";
+            break;
+        case 'e':
             return 0;
-        }
-
-        if (receiving) {
-            
+        case 'n':
+            std::cout << "Enter desired topic name:";
+            std::cin >> topicName;
+            std::cout << "\n";
+            break;
         }
     }
+}
 
-    //INITIALIZING THE PARTICIPANT
+int receiving(int seconds, DDS::DomainParticipantFactory_var dpf, int domainID, string topicName)
+{
     try
     {
         DDS::DomainParticipant_var participant = dpf->create_participant(42, //domain ID
@@ -105,8 +123,7 @@ int main(int argc, char *argv[])
                                            OpenDDS::DCPS::DEFAULT_STATUS_MASK);
         if (!sub)
         {
-        std:
-            cerr << "Failed to create_subscriber." << std::endl;
+            std::cerr << "Failed to create_subscriber." << std::endl;
             return 1;
         }
 
@@ -161,7 +178,7 @@ int main(int argc, char *argv[])
             }
 
             DDS::ConditionSeq conditions;
-            DDS::Duration_t timeout = {60, 0};
+            DDS::Duration_t timeout = {seconds, 0};
             if (ws->wait(conditions, timeout) != DDS::RETCODE_OK)
             {
                 ACE_ERROR_RETURN((LM_ERROR,
@@ -183,6 +200,20 @@ int main(int argc, char *argv[])
         e._tao_print_exception("Exception caught in main():");
         return 1;
     }
-
     return 0;
+}
+
+void printInstructions(int domainID, string username, string topic)
+{
+    std::cout << "\n-------------------------.\n";
+    std::cout << "username: " + username + " | ";
+    std::cout << "dID: " << domainID;
+    std::cout << " | ";
+    std::cout << "topic: " + topic;
+    std::cout << "\n-------------------------.\n";
+    std::cout << "r: Open to receive data.\n";
+    std::cout << "t: Set desired Domain ID.\n";
+    std::cout << "u: Set username.\n";
+    std::cout << "n: Set desired Topic.\n";
+    std::cout << "e: exit subscriber.\n";
 }
